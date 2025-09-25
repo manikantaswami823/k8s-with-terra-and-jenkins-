bc0c1f76c252e6f098da2aa10acabcb108db097b
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 01:26:49 +0530

    Update eks.tf
    updating

--- eks.tf
+++ eks.tf
@@ -1,25 +1,19 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
-  cluster_name                  = local.name
+  cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube_proxy = {
-      most_recent = true
-    }
-    vpc_cni = {
-      most_recent = true
-    }
+    coredns    = { most_recent = true }
+    kube_proxy = { most_recent = true }
+    vpc_cni    = { most_recent = true }
   }
 
-  vpc_id                    = module.vpc.vpc.id
-  subnet_ids                = module.vpc.private_subnets
-  control_plane_subnet_ids  = module.vpc.intra_subnets
+  vpc_id                   = module.vpc.vpc.id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
   eks_managed_node_group_defaults = {
     ami_type                             = "AL2_X86_64"
@@ -29,7 +23,20 @@ module "eks" {
 
   eks_managed_node_groups = {
     k8s-stagingggg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-      instance_types = ["t3.large"]_
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+      tags = {
+        ExtraTag = "Helloworld"
+      }
+    }
+  }
+
+  # Optional global tags for cluster
+  tags = {
+    Environment = "staging"
+    Project     = "eks"
+  }
+}
