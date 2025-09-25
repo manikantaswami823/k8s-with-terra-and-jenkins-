ffeb7865d59aa5c3f3e429ed96ea67e7cc63cb19
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 00:54:43 +0530

    Update provider.tf
    string error (") was added

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 locals{
     region = "us-east-1"
-    name = "eks-staging-cluster
+    name = "eks-staging-cluster"
     vpc_cidr = 10.123.0.0/16"
     azs = ["us-east-1a", "us-east-1b"]
     public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]
@@ -17,4 +17,5 @@ locals{
 
 provider "aws"{
     region = "us-east-1"
+
 }
