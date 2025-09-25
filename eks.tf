88e659b2e4403d70637a042c0edb9f219c555008
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 01:49:17 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -2,23 +2,33 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
-  cluster_name                   = local.name
+  cluster_name                  = local.name
+  cluster_version               = "1.33"                # specify Kubernetes version
   cluster_endpoint_public_access = true
 
   cluster_addons = {
-    coredns   = { most_recent = true }
-    kube_proxy = { most_recent = true }
-    vpc_cni   = { most_recent = true }
+    coredns = {
+      version     = "v1.12.0-eksbuild.1"              # compatible add-on version
+      most_recent = false
+    }
+    kube_proxy = {
+      version     = "v1.32.1-eksbuild.1"
+      most_recent = false
+    }
+    vpc_cni = {
+      version     = "v1.12.2-eksbuild.1"
+      most_recent = false
+    }
   }
 
-  vpc_id                   = module.vpc.vpc_id
+  vpc_id                   = module.vpc.id              # correct output from your VPC module
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
   eks_managed_node_group_defaults = {
-    ami_type                             = "AL2_x86_64"
     instance_types                        = ["m4.large"]
     attach_cluster_primary_security_group = true
+    ami_type                              = "CUSTOM"        # CUSTOM lets EKS choose the correct AL2 for v1.33
   }
 
   eks_managed_node_groups = {
@@ -33,10 +43,4 @@ module "eks" {
       }
     }
   }
-
-  tags = {
-    Environment = "staging"
-    Project     = "eks"
-  }
 }
-
