88fa2c8ae97043b8b9bb264dd3e6ee40fcc61121
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 01:03:21 +0530

    Update vpc.tf
    some errors

--- vpc.tf
+++ vpc.tf
@@ -1,27 +1,27 @@
-module "vpc"{
-    source = "terraform-aws-modules/vpc/aws"
-    version = "~> 4.0"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
 
-    name = local.name
-    cidr = local.vpc_cidr
+  name = local.name
+  cidr = local.vpc_cidr
 
-    azs = local.azs
-    private_subnets = local.private_subnets
-    public_subnets = local.public_subnets
-    intra_subnets = local.intra_subnets
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
 
+  enable_nat_gateway = true
 
-    enable_nat_gateway = true
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
 
-    public_subnet_tags = {
-        "kubernetes.io/role/elb" = 1
-    }
-    
-    private_subnet_tags = {
-        "kubernetes.io/role/internal-elb" = 1
-    }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 
-
-
-
+  tags = {
+    Environment = "staging"
+    Project     = "eks"
+  }
 }
