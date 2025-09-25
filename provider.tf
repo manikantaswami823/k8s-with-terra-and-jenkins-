f4d526ce103ab173598f95d1116dc76500cc8aa8
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 01:14:56 +0530

    Update provider.tf
    changing tf versison

--- provider.tf
+++ provider.tf
@@ -4,21 +4,21 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.67" # pin to 4.x (works with eks 19.x and vpc 4.x)
+      version = "~> 4.67"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = "us-east-1"  # Hardcoded region
 }
 
 locals {
   name      = "eks-staging-cluster"
   vpc_cidr  = "10.123.0.0/16"
-  azs       = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  azs       = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
   private_subnets = ["10.123.1.0/24", "10.123.2.0/24", "10.123.3.0/24"]
   public_subnets  = ["10.123.101.0/24", "10.123.102.0/24", "10.123.103.0/24"]
-  intra_subnets   = ["10.123.201.0/24", "10.123.202.0/24", "10.123.203.0/24"]
+  # intra_subnets removed as VPC module doesn't support this
 }
