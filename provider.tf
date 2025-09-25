166c669aa592153a9c68e551bb57674b5ab7239f
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 01:08:50 +0530

    Update provider.tf
    version issue i have updated the version

--- provider.tf
+++ provider.tf
@@ -1,16 +1,24 @@
-locals {
-  name           = "eks-staging-cluster"
-  vpc_cidr       = "10.123.0.0/16"
-  azs            = ["us-east-1a", "us-east-1b"]
-  public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+terraform {
+  required_version = ">= 1.3.0"
 
-  tags = {
-    Example = local.name
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67" # pin to 4.x (works with eks 19.x and vpc 4.x)
+    }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
+}
+
+locals {
+  name      = "eks-staging-cluster"
+  vpc_cidr  = "10.123.0.0/16"
+  azs       = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+
+  private_subnets = ["10.123.1.0/24", "10.123.2.0/24", "10.123.3.0/24"]
+  public_subnets  = ["10.123.101.0/24", "10.123.102.0/24", "10.123.103.0/24"]
+  intra_subnets   = ["10.123.201.0/24", "10.123.202.0/24", "10.123.203.0/24"]
 }
