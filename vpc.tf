82a3196ee3d6547fbe2328181ea0845a7a506675
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 01:15:40 +0530

    Update vpc.tf
    solved errors

--- vpc.tf
+++ vpc.tf
@@ -8,16 +8,15 @@ module "vpc" {
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/role/elb" = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/internal-elb" = "1"
   }
 
   tags = {
