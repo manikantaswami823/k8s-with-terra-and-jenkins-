e9e8a2c49b9b99e10853f8a330234cb464e7e7fc
manikantaswami karthikeya varma <swami.mahasivabhattu@gmail.com>
2025-09-26 01:55:17 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,26 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "4.0.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "staging-vpc"
+  cidr = "10.0.0.0/16"
 
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
-  }
+  single_nat_gateway = false
 
   tags = {
     Environment = "staging"
     Project     = "eks"
   }
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
 }
